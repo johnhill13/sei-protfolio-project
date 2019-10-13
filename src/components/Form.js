f9a3c9cacd160177339324131d6c322e74ec67a9
John H <johnnyhill13@gmail.com>
2019-10-13 13:06:17 -0700

    adds formspree

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Container, Grid, Button, Form} from 'semantic-ui-react';
+import { Container, Divider, Button, Form} from 'semantic-ui-react';
 
 const form = () => {
     return(
         <Container className='ui inverted secondary center aligned segment'>
-            <Grid centered columns={2}>
-                <Grid.Column>
-                    <Form>
-                        <Form.Input className='ui center' label='Enter your email' name="email"  placeholder='email'/>
-                    </Form>
-                    <Button> Submit </Button>
-                </Grid.Column>
-            </Grid>
+            <Form action='https://formspree.io/hillxjohn@gmail.com' method='POST' size='tiny' key='tiny'>
+                <Form.Group widths='equal'>
+                    <Form.Field
+                        label="Share your Email if you wish to connect."
+                        control='input'
+                        placeholder='Your Email'
+                        type='email'
+                        name='_replyto'
+                    />
+                </Form.Group>
+            <Button type='submit' value='Send'>Submit</Button>
+            <Divider hidden />
+            </Form>
 
         </Container>
     )
 }
 
 export default form;
-
