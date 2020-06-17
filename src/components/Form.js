2768b3fa4ddc0f49a699f4182f8313dbb903192d
John H <johnnyhill13@gmail.com>
2020-06-16 17:03:09 -0700

    updates resume and some design, needs form submission fix still

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,7 +5,7 @@ import {
   Button,
   Form,
   Grid,
-  Input
+  Input,
 } from "semantic-ui-react";
 import "./Form.css";
 
@@ -17,22 +17,57 @@ const form = () => {
         method="POST"
         size="medium"
         key="medium"
+        id="fs-frm"
+        className="simple-contact-form"
+        accept-charset="utf-8"
       >
-          <h3>Please share your email below and I will reach out directly.</h3>
-        <Input
-        //   label="Share your Email if you wish to connect."
-          control="input"
-          placeholder="Your Email"
-          type="email"
-          name="_replyto"
-          className="email-input"
-        />
+        <Form.Group>
+          <h3 className="share-info">Please share your info below and I will reach out to you directly.</h3>
+          <Form.Input
+            fluid
+            type="text"
+            placeholder="Full Name"
+            name="name"
+            className="name-input"
+          />
+            <Form.Input
+              fluid
+              placeholder="Email Address"
+              type="email"
+              name="_replyto"
+              className="email-input"
+            />
+          <Form.Input
+            fluid
+            id="message"
+            placeholder="Comment to John."
+            type="text"
+            rows="3"
+            name="message"
+            className="message-input"
+          />
+          <Button type="submit" value="Send" size="tiny">
+            Submit
+          </Button>
+        </Form.Group>
       </Form>
-      <Button type="submit" value="Send" size="tiny">
-        Submit
-      </Button>
     </Container>
   );
 };
 
 export default form;
+
+{
+  /* <form id="fs-frm" name="simple-contact-form" accept-charset="utf-8" action="https://formspree.io/YOUR_EMAIL_HERE" method="post">
+  <fieldset id="fs-frm-inputs">
+    <label for="full-name">Full Name</label>
+    <input type="text" name="name" id="full-name" placeholder="First and Last" required="">
+    <label for="email-address">Email Address</label>
+    <input type="email" name="_replyto" id="email-address" placeholder="email@domain.tld" required="">
+    <label for="message">Message</label>
+    <textarea rows="5" name="message" id="message" placeholder="Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla nullam quis risus." required=""></textarea>
+    <input type="hidden" name="_subject" id="email-subject" value="Contact Form Submission">
+  </fieldset>
+  <input type="submit" value="Submit">
+</form> */
+}
